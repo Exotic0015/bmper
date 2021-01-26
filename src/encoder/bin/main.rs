a8d433d4bdbb1cc12dadaee7a45e12173b56c5ee
Exotic0015 <jalokim541515@gmail.com>
2021-01-26 20:08:11 +0100

    updates & improvements

--- src/encoder/bin/main.rs
+++ src/encoder/bin/main.rs
@@ -98,7 +98,7 @@ fn main() {
         .to_lowercase();
 
     // Save the image.
-    let png_filename = format! {"{}.png", rname};
+    let png_filename = format! {"{}.{}.png", rname, raw_file_path.extension().unwrap().to_str().unwrap()};
 
     image.save(png_filename).unwrap();
 

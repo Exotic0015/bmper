73f8935b8394e902dc64f44591dd1667f2b1b370
Exotic <jalokim541515@gmail.com>
2022-04-14 00:58:36 +0200

    Delete build.ps1

--- build.ps1
+++
@@ -1,3 +0,0 @@
-$env:RUSTFLAGS="--emit=asm" 
-cargo +nightly build --release -Z build-std --target x86_64-pc-windows-msvc
-#cargo +nightly build --release -Z build-std --target x86_64-unknown-linux-gnu
